e458bfd59b5151c8fb79dcb64abd916c8ed0499e
Shameek Poddar <spoddar@leveldata.com>
2016-05-30 13:36:58 -0400

    Move everything to module.exports

--- api/utils.js
+++ api/utils.js
@@ -1,18 +1,14 @@
 var apiUrl = require('./parameters').url
 
-function buildQueryString(params){
-	var result = [];
-	for(var p in params){
-		result.push(p + '=' + params[p]);
-	}
-	return result.join('&');
-}
-
-function buildUrl(params){
-	return apiUrl + buildQueryString(params)
-}
-
 module.exports = {
-	buildQueryString: buildQueryString,
-	buildUrl: buildUrl
+	buildQueryString: function(params){
+						var result = [];
+						for(var p in params){
+							result.push(p + '=' + params[p]);
+						}
+						return result.join('&');
+					},
+	buildUrl: function(params){
+				return apiUrl + this.buildQueryString(params)
+			}
 }
