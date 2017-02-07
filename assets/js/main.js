f475dcad425e7b2d2f9758e58d1942a9d3a71f4f
Shameek Poddar <shameek.poddar@gmail.com>
2017-02-06 21:49:59 -0500

    Initial refactor to use Backbone

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,61 @@
 $(document).ready(function(){
-	function showResults(data){	
-		//Builds ordered list of results	
-		var results = [];
-		$.each(data, function(index, item){
-			results.push({
-				icon: item.icon,
-				name: item.name,
-				address: item.vicinity
-			});
-		});
-
-		var template = Handlebars.compile($("#results-template").text());
-		var rendered = template({results: results});
 	
-		$("#results").html(rendered);
-	}
-
-	function geoSuccess(p){
-		console.log("Found user's location at " + p.coords.latitude + ", " + p.coords.longitude);
-		
-		var params = {
-			"lat": p.coords.latitude,
-			"long": p.coords.longitude
+	appEvents = _.extend({}, Backbone.Events);
+	
+	var ResultsCollection = Backbone.Collection.extend({
+		initialize: function(models, options){
+			this.latitude = options && options.latitude; 
+			this.longitude = options && options.longitude; 
+		},
+		url: function(){
+			return "/open?" + buildQueryString({
+				"lat": this.latitude,
+				"long": this.longitude
+			}) 
+		},
+		parse: function(data){
+			var data = JSON.parse(data);
+			return data.results;
 		}
-		
-		$.get("/open?" + buildQueryString(params)) //Get response from server
-			.then(JSON.parse)
-			.then(function(data) {
-				if(data.status == 'OK'){
-					showResults(data.results);		
-				} else {
-					$("#results").html("Something went wrong. We're working on it!");
-				}
-			});	
-	}
-
-	function geoError(p){
-		alert("Couldn't find your location. We kind of need that");
-	}
-
-	if(geoPosition.init()){
-		geoPosition.getCurrentPosition(geoSuccess, geoError);
-	}
+	});
+	
+	var ResultsView = Backbone.View.extend({
+		el: "#results",
+		template: Handlebars.compile($("#results-template").text()),
+		initialize: function(options){
+			this.listenTo(this.collection, "change", this.render);
+			this.render();
+		},
+		render: function(){
+			this.$el.html(this.template(this.collection.toJSON()));
+			return this;
+		}
+	});
+	
+	var AppView = Backbone.View.extend({
+		el: "body",
+		geoSuccess: function(p){
+			var view = this;
+			console.log("Found user's location at " + p.coords.latitude + ", " + p.coords.longitude);
+			this.results = new ResultsCollection([], { latitude: p.coords.latitude, longitude: p.coords.longitude });
+			this.results.fetch().done(function(){
+				appEvents.trigger("app:loadResults")
+			});
+		},
+		geoError: function(p){
+			alert("Couldn't find your location. We kind of need that");
+		},
+		initialize: function(){			
+			var view = this;
+			
+			geoPosition.init && geoPosition.init() && geoPosition.getCurrentPosition(this.geoSuccess.bind(this), this.geoError.bind(this));
+			
+			this.listenTo(appEvents, "app:loadResults", function(){
+				view.$("#subheading").css("display", "block"); //Display subheading
+				var resultsView = new ResultsView({ collection: view.results });
+			});
+		}
+	});
+	
+	var app = new AppView(); //Kickstart the app
 });
