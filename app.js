e61f111b0825b6cf2d6f972e5be41c664195d5f6
Shameek Poddar <shameek.poddar@gmail.com>
2017-02-13 20:30:50 -0500

    Force app to redirect to HTTPS on Heroku

--- app.js
+++ app.js
@@ -3,9 +3,25 @@ let request = require('request'),
 	_ = require('underscore'),
 	apiParameters = require('./api/params'),
 	apiUtils = require('./api/utils');
+	
+let env = process.env.NODE_ENV || 'development';
 
 let app = express();
-app.use(express.static('assets'))
+
+//Taken from http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect 
+//Forces redirect to SSL in production so that location sensing API will work automatically
+let forceSsl = (req, res, next) => {
+	if(req.headers['x-forwarded-proto'] !== 'https'){
+		return res.redirect('https://' + req.get('Host') + req.url); 
+	}
+	return next();
+}
+
+if(env === 'production'){
+	app.use(forceSsl);
+}
+
+app.use(express.static('assets')); //Static assets directory
 
 app.get('/', (req, res) => {
 	res.sendfile("index.html");
