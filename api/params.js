ae3ca1f62422317dcad6bd91b902c4d358624b09
Shameek Poddar <shameek.poddar@gmail.com>
2017-02-13 20:39:53 -0500

    Exporting API key

--- api/params.js
+++ api/params.js
@@ -1,6 +1,7 @@
 let api_key =  'AIzaSyAsaKDM1E9cv45rSvphS8hv1X7eKtovbBg';
 
-module.exports = {		
+module.exports = {
+		apiKey: api_key,
 		listUrl: 'https://maps.googleapis.com/maps/api/place/nearbysearch/json?',
 		params: {
 			'key': api_key,
