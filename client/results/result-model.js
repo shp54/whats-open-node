bfa115796828b9698ba50331797a279ad8bad997
Shameek Poddar <shameek.poddar@gmail.com>
2017-04-18 11:11:54 -0700

    No async weirdness, I'm just stupid. Finally added closing time to display

--- client/results/result-model.js
+++ client/results/result-model.js
@@ -1,5 +1,6 @@
 let Backbone = require('backbone'),
 	$ = require('jquery'),
+	_ = require('underscore'),
 	moment = require('moment'),
 	apiParams = require('../../api/params')
 
@@ -13,9 +14,16 @@ let ResultModel = Backbone.Model.extend({
 			let result = data.result
 			if(result && result.opening_hours){
 				let currentWeekday = moment().day()
-				let hoursToday = _.find(result.opening_hours.periods, (p) => p.open.day == weekday)
+				let hoursToday = {}
+				if(result.opening_hours.periods.length > 0){ //if there's more than one period, find the one for the current weekday
+					hoursToday = _.find(result.opening_hours.periods, (p) => p.open.day == currentWeekday)
+				} else { //otherwise just find the one they got
+					hoursToday = result.opening_hours.periods[0]
+				}
 				console.log(hoursToday)
-				this.set('closingTime', moment(hoursToday.close.time, 'HH').format('LT'))
+				if(hoursToday.close){
+					this.set('closingTime', moment(hoursToday.close.time, 'HH').format('LT'))
+				}
 			}
 		})
 	}
