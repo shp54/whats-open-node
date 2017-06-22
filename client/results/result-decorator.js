7dbe2404c01344b9fbb1a334c9695d8ed4d45465
Shameek Poddar <shameek.poddar@gmail.com>
2017-06-22 11:03:43 -0400

    Break decorator code into separate functions

--- client/results/result-decorator.js
+++ client/results/result-decorator.js
@@ -2,7 +2,14 @@ let _ = require('underscore'),
 	moment = require('moment')
 
 //This module is used to decorate the JSON returned by the result module (cleans up model details)
-let ResultDecorator = {
+let ResultDecorator = {	
+	getPlaceUrl(data){
+		if(data.result){ //Model doesn't have API results right away, so you have to check if it's complete
+			return data.result.url
+		} else {
+			return ""
+		}
+	},
 	getHoursForWeekday(periods, weekday){
 		if(periods.length == 1){ //if there's just one period, return the one they got
 			return periods[0]
@@ -10,19 +17,23 @@ let ResultDecorator = {
 			return _.find(periods, (p) => p.open.day == weekday)
 		}
 	},
+	getOpeningHours(data){
+		if(data.result && data.result.opening_hours){ //Get opening hours for current weekday
+			let result = data.result
+			let currentWeekday = moment().day()
+			let hoursToday = this.getHoursForWeekday(result.opening_hours.periods, currentWeekday)
+			if(hoursToday.close){
+				return moment(hoursToday.close.time, 'HH').format('LT')
+			}
+		} else {
+			return ""
+		}
+	},
 	decorate(data){
 		let decs = {};
-		if(data.result){ //Model doesn't have API results right away, so you have to check if it's complete
-			let result = data.result
-			decs.placeUrl = result.url; //Get maps URL
-			if(data.result.opening_hours){ //Get opening hours for current weekday
-				let currentWeekday = moment().day()
-				let hoursToday = this.getHoursForWeekday(result.opening_hours.periods, currentWeekday)
-				if(hoursToday.close){
-					decs.closingTime = moment(hoursToday.close.time, 'HH').format('LT')
-				}
-			}
-		}
+		
+		decs.placeUrl = this.getPlaceUrl(data); //Get maps URL
+		decs.closingTime = this.getOpeningHours(data);
 		
 		return _.extend(data, decs)
 	}
