efaa9e012c04b94a2b0f3d34d64f96f13f497508
Shameek Poddar <shameek.poddar@gmail.com>
2017-10-29 14:01:38 -0400

    Fix whitespace

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let request = require('request-promise'),
-	express = require('express'),
-	memjs = require('memjs'),
-	_ = require('underscore'),
-  Promise = require('bluebird'),
-	apiParameters = require('./api/params')
+    express = require('express'),
+    memjs = require('memjs'),
+    _ = require('underscore'),
+    Promise = require('bluebird'),
+    apiParameters = require('./api/params')
 
 let env = process.env.NODE_ENV || 'development';
 
@@ -16,37 +16,37 @@ let app = express();
 //Taken from http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect 
 //Forces redirect to SSL in production so that location sensing API will work automatically
 let forceSsl = (req, res, next) => {
-	if(req.headers['x-forwarded-proto'] !== 'https'){
-		return res.redirect('https://' + req.get('Host') + req.url); 
-	}
-	return next();
+  if(req.headers['x-forwarded-proto'] !== 'https'){
+    return res.redirect('https://' + req.get('Host') + req.url); 
+  }
+  return next();
 }
 
 if(env === 'production'){
-	app.use(forceSsl);
+  app.use(forceSsl);
 }
 
 app.use(express.static('assets')); //Static assets directory
 
 app.get('/', (req, res) => {
-	res.sendfile("index.html");
+  res.sendfile("index.html");
 });
 
 app.get('/open', (req, res) => {
-	let { lat, long } = req.query,
+  let { lat, long } = req.query,
       options = { 
         url: apiParameters.listUrl,
         qs: _.extend(apiParameters.params, { location: `${lat},${long}` })
       }
-		
-	request(options).then((response) => {
-			res.setHeader('content-type', 'application/json'); 
-			res.send(response);
+    
+  request(options).then((response) => {
+    res.setHeader('content-type', 'application/json'); 
+    res.send(response);
   });
 });
 
 app.get('/place/:placeId', (req, res) => {
-	let placeid = req.params.placeId,
+  let placeid = req.params.placeId,
       options = { 
         uri: apiParameters.placeUrl,
         qs: {
@@ -54,23 +54,23 @@ app.get('/place/:placeId', (req, res) => {
           key: apiParameters.apiKey
         }
       }
-	
-	cacheGet(placeid).then((val) => {
-		if(val){
+
+  cacheGet(placeid).then((val) => {
+    if(val){
       return val.toString()
-		} else {
-			return request(options).then((response) => {
-					cacheSet(placeid, response, {});
+    } else {
+      return request(options).then((response) => {
+          cacheSet(placeid, response, {});
       });
     }
   }).then((response) => {
-      res.setHeader('content-type', 'application/json'); 
-			res.send(response);		
+    res.setHeader('content-type', 'application/json'); 
+    res.send(response);		
   });
 });
 
 let server = app.listen(process.env.PORT || 3000, () => {
-	let port = server.address().port;
-	
-	console.log('App listening on port %s', port);
+  let port = server.address().port;
+
+  console.log('App listening on port %s', port);
 });
