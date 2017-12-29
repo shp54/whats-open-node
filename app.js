e2aee3b44f69ffec7b01c6481b1db03d1f1f3fa9
Shameek Poddar <shameek.poddar@gmail.com>
2017-12-28 20:05:47 -0500

    Switch from Express to Hapi

--- app.js
+++ app.js
@@ -1,85 +1,86 @@
-let fetch = require('node-fetch'),
-    querystring = require('querystring'),
-    express = require('express'),
-    memjs = require('memjs'),
-    Promise = require('bluebird'),
-    apiParameters = require('./api/params')
+const fetch = require('node-fetch');
+const querystring = require('querystring');
+const hapi = require('hapi');
+const inert = require('inert');
+const memjs = require('memjs');
+const Promise = require('bluebird');
+const apiParameters = require('./api/params')
 
-let env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'development';
 
-let cache = memjs.Client.create(),
-    cacheSet = Promise.promisify(cache.set, {context: cache}),
-    cacheGet = Promise.promisify(cache.get, {context: cache})
+const cache = memjs.Client.create();
+const cacheSet = Promise.promisify(cache.set, {context: cache});
+const cacheGet = Promise.promisify(cache.get, {context: cache});
 
 const makeUrl = (url, paramsObj) => url + '?' + querystring.stringify(paramsObj);
 
-let app = express();
-
-//Taken from http://stackoverflow.com/questions/7185074/heroku-nodejs-http-to-https-ssl-forced-redirect 
-//Forces redirect to SSL in production so that location sensing API will work automatically
-let forceSsl = (req, res, next) => {
-  if(req.headers['x-forwarded-proto'] !== 'https'){
-    return res.redirect('https://' + req.get('Host') + req.url); 
-  }
-  return next();
-}
-
-if(env === 'production'){
-  app.use(forceSsl);
-}
-
-app.use(express.static('assets')); //Static assets directory
-
-app.get('/', (req, res) => {
-  res.sendfile("index.html");
+const server = hapi.server({
+  port: 3000,
 });
 
-app.get('/open', (req, res) => {
-  let { lat, long } = req.query,
-      qs = { 
-	key: apiParameters.apiKey, 
-	location: `${lat},${long}`,
-	type: 'restaurant',
-	rankby: 'distance',
-	opennow: 'true',
-      };
-    
-  fetch(makeUrl(apiParameters.listUrl, qs))
-  .then(result => result.json())
-  .then(response => {
-    res.setHeader('content-type', 'application/json'); 
-    res.send(response);
+const startServer = async () => {
+  await server.register(inert)
+
+  server.route({
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './assets',
+        redirectToSlash: true,
+        index: true,
+      },
+    },
   });
-});
 
-app.get('/place/:placeId', (req, res) => {
-  let placeid = req.params.placeId,
-      qs = {
+  server.route({
+    method: 'GET',
+    path: '/open',
+    handler: (request, h) => {
+      const { lat, long } = request.query;
+      const location = `${lat},${long}`;
+      console.log(`Request sent from ${location}`);
+      const qs = {
+        key: apiParameters.apiKey,
+        location,
+        type: 'restaurant',
+        rankby: 'distance',
+        opennow: 'true',
+      };
+
+      return fetch(makeUrl(apiParameters.listUrl, qs)).then(result => result.json());
+    },
+  });
+
+  server.route({
+    method: 'GET',
+    path: '/place/{placeid}',
+    handler: (request, h) => {
+      const { placeid } = request.params;
+      const qs = {
           placeid,
           key: apiParameters.apiKey,
       };
 
-  const fetchPlace = fetch(makeUrl(apiParameters.placeUrl, qs))
-			  .then(result => result.json());
+      const fetchPlace = fetch(makeUrl(apiParameters.placeUrl, qs)).then(result => result.json());
 
-  cacheGet(placeid).then((val) => {
-    if(val){
-      return val.toString()
-    } else {
-      return fetchPlace.then(response => {
-        cacheSet(placeid, response, {})
-        return response
-      });
-    }
-  }).catch(err => fetchPlace)
-  .then(response => {
-    res.setHeader('content-type', 'application/json'); 
-    res.send(response);		
+      return cacheGet(placeid)
+            .then(val => {
+              if(!val){
+                return fetchPlace.then(response => {
+                  cacheSet(placeid, JSON.stringify(response), {});
+                  return response;
+                });
+              } else {
+                return JSON.parse(val.toString());
+              }
+            })
+            .catch(err => fetchPlace);
+    },
   });
-});
 
-let server = app.listen(process.env.PORT || 3000, () => {
-  let port = server.address().port;
+  await server.start();
+  console.log('Server started on port 3000!');
+};
 
-  console.log('App listening on port %s', port);
-});
+startServer();
