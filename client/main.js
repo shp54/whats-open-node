7c1f75801a8be8cf3b47eba85c2f4a11a6bdb4d5
Shameek Poddar <spoddar@operasolutions.com>
2017-12-29 14:13:01 -0500

    Basic app logic out of the way - todo presentation and then more complicated logic

--- client/main.js
+++ client/main.js
@@ -1,75 +1,36 @@
 const { h, app } = require('hyperapp');
+const geoPosition = require('../lib/geoPosition');
 
 const state = { 
  isLoading: true,
- lat: 42.3565551,
- long: -71.18526419999999,
+ latitude: null,
+ longitude: null,
  results: [],
 };
 
 const actions = {
-  setResults: results => (state, actions) => ({ results }),
   setLoading: isLoading => (state, actions) => ({ isLoading }),
+  setLocation: ({ latitude, longitude }) => (state, actions) => ({ latitude, longitude }),
+  finishFetch: results => (state, actions) => ({ isLoading: false, results }),
   fetchData: () => (state, actions) => { 
     actions.setLoading(true);
-    fetch(`/open?lat=${state.lat}&long=${state.long}`)
+    fetch(`/open?lat=${state.latitude}&long=${state.longitude}`)
       .then(res => res.json())
-      .then(data => {
-	actions.setLoading(false);
-	actions.setResults(data.results);
-      });
+      .then(data => actions.finishFetch(data.results));
   },
 };
 
 const view = (state, actions) => h("div", {}, [
-  h("div", {}, state.loading ? "Loading..." : "Done"),
+  h("div", {}, [
+   state.isLoading ? h("img", {src: 'images/spinner.gif'}) : null
+  ]),
   h("div", {}, JSON.stringify(state.results)),
 ]);
 
 const main = app(state, actions, view, document.body);
 
-main.fetchData();
-/* let $ = require('jquery'),
-	Backbone = require('backbone'),
-	ResultsView = require('./results/results-view'),
-	ResultsCollection = require('./results/results-collection'),
-	geoPosition = require('../lib/geoPosition');
-	
-let AppView = Backbone.View.extend({
-	el: "body",
-	geoSuccess(p){
-		let { latitude, longitude } = p.coords;
-		this.loadResults(latitude, longitude)
-		setTimeout(() => {
-			this.loadResults(latitude, longitude)
-		}, 60000); 
-	},
-	results: new ResultsCollection(),
-	geoError(p){
-		alert("Couldn't find your location. We kind of need that");
-	},
-	loadResults(latitude, longitude){
-		if(!this.results.latitude){
-			this.results.latitude = latitude;
-		}
-		if(!this.results.longitude){
-			this.results.longitude = longitude;
-		}
-		this.results.fetch() //Fetch automatically "smart updates" the collection, so it will always correctly rerender when something gets added or removed
-	},
-	initialize(){		
-		geoPosition.init && geoPosition.init() && geoPosition.getCurrentPosition((p) => this.geoSuccess(p), (p) => this.geoError(p));
-		this.$("#spinner").show();
-		this.listenTo(this.results, "sync", () => {
-			this.$("#spinner").hide();
-			this.$("#subheading").css("display", "block"); //Display subheading
-		});
-		let resultsView = new ResultsView({ collection: this.results });
-	}
-});
-	
-function init(){
-	let app = new AppView(); //Kickstart the app
-}
-
-$(() => { init() }); //Here we go! */
+geoPosition.init();
+geoPosition.getCurrentPosition(p => {
+  main.setLocation(p.coords);
+  main.fetchData();
+}, p => console.log('Error :('));
