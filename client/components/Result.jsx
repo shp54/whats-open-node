42b00f35c3a3a4c3e552e9a29aa7d31ae2b7ea30
Shameek Poddar <shameek.poddar@gmail.com>
2017-12-29 19:45:18 -0500

    Ugly CSS hacks

--- client/components/Result.jsx
+++ client/components/Result.jsx
@@ -2,12 +2,10 @@ const { h } = require('hyperapp');
 
 const Result = ({ place }) => (
   <li class='list-group-item'>
-  {place.placeUrl ?
-    <a href={place.placeUrl} target='_blank'><h3>{place.name}</h3></a>
-  	: <h3>{place.name}</h3>}
+    <img src={place.icon} alt={place.types[0]} title={place.types[0]} class='icon' width='53' height='53' />
+    {place.placeUrl ?  <a href={place.placeUrl} target='_blank'><h3>{place.name}</h3></a> : <h3>{place.name}</h3>}
     <span class='address'>{place.vicinity}</span>
     {place.closingTime ? <div class='closingTime'>Open until {place.closingTime} today</div> : null}
-    {/*<img src={place.icon} alt={place.types[0]} title={place.types[0]} class='icon' width='53' height='53' />*/}
   </li>
 );
 
