6efd1bc923e0e2ac3db4fc9f6076bf52eb7f807b
Shameek Poddar <shameek.poddar@gmail.com>
2017-12-29 19:30:46 -0500

    Move component to separate file

--- client/index.js
+++ client/index.js
@@ -1,16 +1,6 @@
 const { h, app } = require('hyperapp');
 const geoPosition = require('../lib/geoPosition');
-
-const Result = ({ place }) => (
-  <li class='list-group-item'>
-  {place.placeUrl ?
-    <a href={place.placeUrl} target='_blank'><h3>{place.name}</h3></a>
-  	: <h3>{place.name}</h3>}
-    <span class='address'>{place.vicinity}</span>
-    {place.closingTime ? <div class='closingTime'>Open until {place.closingTime} today</div> : null}
-    {/*<img src={place.icon} alt={place.types[0]} title={place.types[0]} class='icon' width='53' height='53' />*/}
-  </li>
-);
+const Result = require('./components/Result.jsx')
 
 const state = {
  isLoading: true,
