9f9993438e5fff59370cd83edc19e9b370a323b7
Shameek Poddar <shameek.poddar@gmail.com>
2018-01-14 16:43:07 -0500

    Set loading to true so that site doesn't look broken at first.

--- client/index.js
+++ client/index.js
@@ -7,7 +7,7 @@ require("./styles/bootstrap.css"); // Using custom version of bootstrap w/o font
 require("./styles/main.css")
 
 const state = {
- isLoading: false,
+ isLoading: true,
  latitude: null,
  longitude: null,
  results: {},
@@ -23,7 +23,7 @@ const actions = {
       .then(data => actions.addResult(data.result)); // addResult can smart update the entire state
   },
   fetchList: () => (state, actions) => {
-    actions.setLoading(true);
+    state.loading != true && actions.setLoading(true);
     fetch(`/open?lat=${state.latitude}&long=${state.longitude}`)
       .then(res => res.json())
       .then(data => {
